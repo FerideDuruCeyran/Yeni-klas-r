9caa1c305c98d134377ed76f1b8b89f1098e46a6
FerideDuruCeyran <duruceyran@gmail.com>
2023-06-03 21:43:19 +0300

    ÜRÜN DÜZENLEME

---
+++ WebApplication.pr/Eticaret.Model/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Eticaret.Model
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ WebApplication.pr/Eticaret.Model/Products.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Eticaret.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double Price { get; set; }
+
+    }
+}
