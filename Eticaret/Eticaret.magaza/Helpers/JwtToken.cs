3576344659027387e5d2753ed93d34aa3b5d74e8
FerideDuruCeyran <duruceyran@gmail.com>
2023-07-28 21:51:11 +0300

    hi2

---
+++ Eticaret/Eticaret.magaza/Helpers/JwtToken.cs
@@ -0,0 +1,32 @@
+﻿using Eticaret.Model;
+using Microsoft.IdentityModel.Tokens;
+using System.Text.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Eticaret.magaza.Helpers
+{
+    public class JwtToken
+    {
+        public Login Decrypt(string token)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            string key = configuration["jwt.Key"]!;
+            var tokenHandler = new JwtSecurityTokenHandler();
+            token = token.Replace("\\", "");
+            token = token.Replace('"', ' ');
+            token = token.Trim();
+            var jwtTokenWithoutValidation = (JwtSecurityToken)tokenHandler.ReadToken(token);
+            var email = jwtTokenWithoutValidation!.Payload["Email"].ToString();
+            Login user = new()
+            {
+                Email = email!
+            };
+            return user;
+        }
+    }
+}
